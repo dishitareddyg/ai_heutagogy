33f12447c9f1a1bc4319eccd1418791f36b0649c
Disha Bhat <dishambhat06@gmail.com>
2025-12-24 20:08:08 +0530

    Teacher can view the saved roadmaps now

--- teacher/urls.py
+++ teacher/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("plan/", views.create_plan, name="teacher-plan"),
     #path("roadmap/", views.teacher_roadmap, name="teacher-roadmap"),
     path("dashboard/", views.teacher_dashboard, name="teacher-dashboard"),
+    #path("plan/", views.create_plan, name="teacher-plan"),
+    path("plans/", views.view_plans, name="view-plans"),
+    path("plans/<int:plan_id>/", views.plan_detail, name="plan-detail"),
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -9,6 +9,29 @@ import json
 from .services import generate_teacher_roadmap
 from .models import CoursePlan
 
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from .models import CoursePlan
+
+def view_plans(request):
+    plans = CoursePlan.objects.order_by("-created_at")
+
+    paginator = Paginator(plans, 5)  # 5 plans per page
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "teacher/view_plans.html", {
+        "page_obj": page_obj
+    })
+
+def plan_detail(request, plan_id):
+    plan = get_object_or_404(CoursePlan, id=plan_id)
+
+    return render(request, "teacher/plan_detail.html", {
+        "plan": plan
+    })
+
+
 
 def teacher_dashboard(request):
     return render(request, "teacher/dashboard.html")
